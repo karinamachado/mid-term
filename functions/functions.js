fa30a278116a463841720722b8829257593b8c09
Karina Machado <karinamachado@msn.com>
2016-08-26 00:58:47 -0300

    self-assesment week3functions.js, mostly complete

--- functions/functions.js
+++ functions/functions.js
@@ -19,10 +19,65 @@ console.log(whatForLunch('karina', 'apple'));
 
 // isLargerThan100
 
+var isLargerThan100 = function(){
+
+	var state = 100;
+	if(state > 100){
+		console.log('ok is true');
+	}else{
+		console.log('is false, no large than 100');
+	}
+
+	return isLargerThan100
+
+};
 // anyEvens
+
+var anyEvens = function(){
+ 
+
+
+}
 
 // printDataType
 
+
+
 // isDivisibleBy
 
+/*Create a function that takes in two numbers, 
+and returns whether the first number is evenly divisible by the second 
+number. If it is return true, if not return false.
+*/
+
+var isDivisibleBy = function(num1, num2){
+var output = " ";	
+
+if(num1 % num2 ==0){
+	output += " true";
+}else{
+	output += " false ";
+	console.log(isDivisibleBy(10,5));
+}
+
+
 // fizzBuzz
+
+for(var n = 1; n < 100; n++){
+	var output = " ";
+	if(n % 3 == 0)
+	 output +=	"Fizz' if it can be divided evenly by 3 ";
+	if( n % 5 == 0)
+		output += "'Buzz' if it can be divided evenly by 5";
+	console.log(output || n);
+}
+
+
+
+
+
+
+
+
+
+
