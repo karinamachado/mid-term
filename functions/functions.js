da4a3265bed3387d4668b79aaf3906f2c4850c68
Karina Machado <karinamachado@msn.com>
2016-08-25 23:37:02 -0300

    self-assesment week3 functions.js

--- functions/functions.js
+++ functions/functions.js
@@ -2,7 +2,20 @@
 
 // multiply
 
+var multiply = function ( num1, num2){
+
+  return num1*num2;
+};
+console.log(multiply(10,5));
+
 // whatForLunch
+
+var whatForLunch = function( name, food){
+
+	return name + 'had' + food + 'for lunch today'
+
+}
+console.log(whatForLunch('karina', 'apple'));
 
 // isLargerThan100
 
